42b7b9b8b77ca007d799775bb39fca19a552666b
Andraz Bajt <andraz@bajt.me>
2016-12-24 18:06:08 +0100

    Automate test-wait

--- test/Main.hs
+++ test/Main.hs
@@ -2,9 +2,11 @@ module Main where
 
 import Test.Framework (defaultMain)
 import TestVariable (testVariable)
+import TestWait (testWait)
 
 
 main :: IO ()
 main = defaultMain
     [ testVariable
+    , testWait
     ]
